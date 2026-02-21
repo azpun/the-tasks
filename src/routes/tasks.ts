d6a8627ce2d19c582f438b7991afe6ff86c5a6bd
azpun <azpunari@gmail.com>
2026-02-21 09:00:48 +0700

    feat: menambahkan route post untuk users dan tasks

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -4,11 +4,21 @@ import { logger } from '../utils/logger.ts'
 
 export const TaskRouter = Router()
 
+// get tasks data
 TaskRouter.get('/', (req: Request, res: Response) => {
   logger.info('Get Tasks Data Success')
   res.status(200).send({
     message: 'Tasks',
     status: 200,
     data: tasks
+  })
+})
+
+TaskRouter.post('/', (req: Request, res: Response) => {
+  logger.info('Add New Task Success')
+  res.status(200).send({
+    message: 'Users',
+    status: 200,
+    data: req.body
   })
 })
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,11 +4,22 @@ import { logger } from '../utils/logger.ts'
 
 export const UserRouter = Router()
 
+// get users data
 UserRouter.get('/', (req: Request, res: Response) => {
   logger.info('Get Users Data Success')
   res.status(200).send({
     message: 'Users',
     status: 200,
     data: users
+  })
+})
+
+// add new users
+UserRouter.post('/', (req: Request, res: Response) => {
+  logger.info('Add New User Success')
+  res.status(200).send({
+    message: 'Users',
+    status: 200,
+    data: req.body
   })
 })
